593168a8dace738d4b2ab19ff66964646df033ee
Bernard Maire <b.maire@yahoo.fr>
2018-12-03 14:25:03 +0100

    Initial Commit

---
+++ Lecteur Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lecteur Youtube
+//
+//  Created by Bernard Maire on 03.12.18.
+//  Copyright © 2018 Bernard Maire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
